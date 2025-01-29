829cccd9cd65c21f9fe3d615f9138460491518b0
sudeep500 <sudipta13.chakraborty@gmail.com>
2025-01-29 19:02:50 +0530

    change the backend deploy link

--- frontend/service/helper.js
+++ frontend/service/helper.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = "http://localhost:5000";
-// const BACKEND_URL = "https://ds-innovtech.vercel.app";
+// const BACKEND_URL = "http://localhost:5000";
+const BACKEND_URL = "https://ds-innovtech.vercel.app";
 
 export default BACKEND_URL;
